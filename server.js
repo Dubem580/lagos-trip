6c42eb55d55f27543014c9224cbaee0295c63076
Dubem <dubem@MacBook-Pro-4.local>
2019-01-31 13:18:20 -0500

    finished - simple rest api that tests and keeps track of trip expexpenses

--- server.js
+++ server.js
@@ -1,11 +1,107 @@
 const express = require('express')
 const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost'
 
 const app = express()
+app.use(express.json())
 
-app.post('/trip'), (req, res) => {/* */ }
-app.get('trips'), (req, res) => {/* */ }
-app.post('/expense'), (req, res) => {/* */}
-app.get('expenses'), (req, res) => {/* */}
+let db, trips, expenses
+
+mongo.connect(
+  url, {
+    	useNewUrlParser: true
+    },
+  (err, client) => {
+    if(err) {
+	console.error(err)
+	return
+    }
+    db = client.db('tripcost')
+    trips = db.collection('trips')
+    expenses = db.collection('expenses')
+    }
+)
+
+app.post('/trip', (req, res) => 
+{
+	const name = req.body.name
+	trips.insertOne({ name: name}, (err, result) => {
+			if (err) {
+				console.error(err)
+				res.status(500).json(
+					{
+						err: err
+					}
+				)
+				return
+			}
+				res.status(200).json(
+					{ 
+						ok: true
+					}
+				)
+	})
+})
+
+app.get('/trips', (req, res) => {
+	trips.find({trip: req.body.name}).toArray((err, items) =>
+	{
+		if(err){
+			console.error(err)
+			res.status(500).json({
+				err: err
+			})
+			return
+		} 
+		console.log(items)
+		res.status(200).json({
+			trips: items
+		})
+
+	})
+})
+
+app.post('/expense', (req, res) => {
+	expenses.insertOne({
+		trip: req.body.trip,
+		date: req.body.date,
+		amount: req.body.amount,
+		category: req.body.category,
+		description: req.body.description
+	},
+	(err, result) => {
+		if(err){
+			console.error(err)
+			res.status(500).json({
+				err: err
+			})
+			return
+		}
+		console.log(result)
+		res.status(200).json({
+			ok: true
+		})
+	}
+	)
+})
+
+app.get('/expenses', (req, res) => {
+	expenses.find({trip: req.body.trip}).toArray((err, items) =>{
+		if(err) {
+			console.error(err)
+			res.status(500).json({
+				err: err
+			})
+			return
+		}
+		res.status(200).json({
+			trips: items
+		})
+
+	})
+
+})
+
+{ useNewUrlParser: true }
 
 app.listen(3000, () => console.log('server ready'))
